c2e80ce0bb20d8b8448beef84a55155af97b99ae
thinhnguyenuni <thinhnguyenunicore@gmail.com>
2024-11-23 16:38:04 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -19,7 +19,7 @@ public class UserController {
     @RequestMapping("/")
     public String getHomePage() {
         String test = this.userService.handleHello();
-        return "eric.html";
+        return "hello";
     }
 }
 
