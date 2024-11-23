bd9e5c512e371edf4f5061130d72b04353514319
thinhnguyenuni <thinhnguyenunicore@gmail.com>
2024-11-23 15:23:50 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -4,16 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+//include >< exclude
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaptopshopApplication.class, args);
 
 		// container
-		// ApplicationContext hoidanit = SpringApplication.run(LaptopshopApplication.class, args);
+		// ApplicationContext hoidanit =
+		// SpringApplication.run(LaptopshopApplication.class, args);
 		// for (String s : hoidanit.getBeanDefinitionNames()) {
-		// 	System.out.println(s);
+		// System.out.println(s);
 		// }
 	}
 
