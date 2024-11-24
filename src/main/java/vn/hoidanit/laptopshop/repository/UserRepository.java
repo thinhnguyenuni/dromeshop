6d2cff7b0e9de5d532ee34ca306f691a6b9b8d14
thinhnguyenuni <thinhnguyenunicore@gmail.com>
2024-11-24 10:26:40 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -13,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findOneByEmail(String email);
 
     List<User> findAll();
+
+    User findById(long id); // null
 }
