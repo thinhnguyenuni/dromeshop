63b4a7e5ec88c23fd06a6a68c2ed299946a882c8
thinhnguyenuni <thinhnguyenunicore@gmail.com>
2024-12-04 21:14:59 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -74,4 +74,9 @@ public class HomePageController {
         return "client/auth/login";
     }
 
+    @GetMapping("/access-deny")
+    public String getDenyPage(Model model) {
+        return "client/auth/deny";
+    }
+
 }
